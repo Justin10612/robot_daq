31ec0a0e46c137a992a53a558ad5240032efb799
Justin10612 <atom.9031@gmail.com>
2023-11-04 22:31:37 +0800

    Fix Bug which i can't change mode.

--- robot_daq/daq_depth_pid.py
+++ robot_daq/daq_depth_pid.py
@@ -13,9 +13,10 @@ class PidDAQ(Node):
     depth = []
     linear_x = []
     time_list  = []
+    depth_data = 0.0
+    output_x = 0.0
     mode = ''
-    last_mode = ''
-    
+    rec_flag = False
 
     def __init__(self):
         super().__init__('PID_DAQ')
@@ -29,34 +30,44 @@ class PidDAQ(Node):
 
     def mode_callback(self, msg):
         self.mode = msg.data
-        if self.mode == 'FOLLOW':             
-            if self.mode != self.last_mode:
+        # Enter Follow Mode, Start Recording.
+        if self.mode == 'FOLLOW':
+            # The First Time Enter Follow Mode
+            if self.rec_flag == False:
                 self.start_time = round(time.time(), 2)
                 self.get_logger().info('Start')
+            # Record Data and Time
             current_time = round(time.time(), 2)
             self.time_list.append(current_time-self.start_time)
-
-        if self.mode != self.last_mode and self.mode != 'FOLLOW':
-            # plt.plot(self.time_list, self.depth, 'red')
-            plt.plot(self.time_list, self.linear_x, 'blue')
-            plt.xlabel('Time')
-            plt.ylabel('Output')
-            plt.title('Depth PID Respone')
-            plt.show()
-            self.depth = []
-            self.linear_x = []
-            self.time_list = []
-            self.get_logger.info('Stop')
-        
-        self.last_mode = msg.data
+            self.depth.append(self.depth_data)
+            self.linear_x.append(self.output_x)
+            self.rec_flag = True
+            plt.clf()
+        # Exit Follow Mode
+        else:
+            if  self.rec_flag == True:
+                self.get_logger().info('Stop')
+                plt.plot(self.time_list, self.depth, label='Depth', color='red')
+                plt.plot(self.time_list, self.linear_x, label='Output_x', color='blue')
+                plt.xlabel('Time')
+                plt.ylabel('Output')
+                plt.title('Depth PID Respone')
+                plt.legend(loc='upper left')
+                plt.show()
+                self.get_logger().info('Plot Closed')
+                self.depth = []
+                self.linear_x = []
+                self.time_list = []
+                self.rec_flag = False
 
     def pose_callback(self, msg):
         if self.mode == 'FOLLOW':
-            self.depth.append(float(msg.y))
+            #FOLLOW
+            self.depth_data = msg.y
 
     def cmd_callback(self, msg):
         if self.mode == 'FOLLOW':
-            self.linear_x.append(float(msg.linear.x))
+            self.output_x = msg.linear.x
 
 def main(args=None):
     rclpy.init(args=args)
