9ae5014af0bed339a941ab44c9559e5bb3d11c97
Justin10612 <atom.9031@gmail.com>
2023-11-09 21:05:02 +0800

    Add angle pid daq

--- robot_daq/daq_angle_pid.py
+++ robot_daq/daq_angle_pid.py
@@ -8,18 +8,18 @@ from geometry_msgs.msg import Vector3, Twist
 from std_msgs.msg import String
 
 
-class DEPTH_PID_DAQ(Node):
+class ANGLE_PID_DAQ(Node):
 
-    depth = []
-    linear_x = []
+    angle = []
+    angular_z = []
     time_list  = []
-    depth_data = 0.0
-    output_x = 0.0
+    angle_data = 0.0
+    output_z = 0.0
     mode = ''
     rec_flag = False
 
     def __init__(self):
-        super().__init__('DEPTH_PID_DAQ')
+        super().__init__('ANGLE_PID_DAQ')
         self.pose_sub = self.create_subscription(Vector3, 'human_pose', self.pose_callback, 10)
         self.pose_sub  # prevent unused variable warning
         self.cmd_sub = self.create_subscription(Twist, 'follow_cmd_vel', self.cmd_callback, 10)
@@ -39,39 +39,39 @@ class DEPTH_PID_DAQ(Node):
             # Record Data and Time
             current_time = round(time.time(), 2)
             self.time_list.append(current_time-self.start_time)
-            self.depth.append(self.depth_data)
-            self.linear_x.append(self.output_x)
+            self.angle.append(self.angle_data)
+            self.angular_z.append(self.output_z)
             self.rec_flag = True
             plt.clf()
         # Exit Follow Mode
         else:
             if  self.rec_flag == True:
                 self.get_logger().info('Stop')
-                plt.plot(self.time_list, self.depth, label='Depth', color='red')
-                plt.plot(self.time_list, self.linear_x, label='Output_x', color='blue')
+                plt.plot(self.time_list, self.angle, label='Angle', color='red')
+                plt.plot(self.time_list, self.angular_z, label='Output_z', color='blue')
                 plt.xlabel('Time')
                 plt.ylabel('Output')
-                plt.title('Depth PID Respone')
+                plt.title('Angle PID Respone')
                 plt.legend(loc='upper left')
                 plt.show()
                 self.get_logger().info('Plot Closed')
-                self.depth = []
-                self.linear_x = []
+                self.angle = []
+                self.angular_z = []
                 self.time_list = []
                 self.rec_flag = False
 
     def pose_callback(self, msg):
         if self.mode == 'FOLLOW':
             #FOLLOW
-            self.depth_data = msg.y
+            self.angle_data = (msg.x-640)*0.07
 
     def cmd_callback(self, msg):
         if self.mode == 'FOLLOW':
-            self.output_x = msg.linear.x
+            self.output_z = msg.angular.z
 
 def main(args=None):
     rclpy.init(args=args)
-    pid_daq = DEPTH_PID_DAQ()
+    pid_daq = ANGLE_PID_DAQ()
     rclpy.spin(pid_daq)
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'robot_daq'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.1.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -20,7 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'daq_depth_pid = robot_daq.daq_depth_pid:main',
+            'depth_pid_daq = robot_daq.daq_depth_pid:main',
+            'angle_pid_daq = robot_daq.daq_angle_pid:main',
         ],
     },
 )
